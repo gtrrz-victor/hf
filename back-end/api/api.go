ff81ad6d4028c72c31a1dc6be1ae0cdc8e8e13c5
Victor Gutierrez Calderon <gtrrz.victor@gmail.com>
2021-10-08 14:43:40 +1100

    add  middlewares

--- back-end/api/api.go
+++ back-end/api/api.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
@@ -9,7 +10,17 @@ import (
 
 func StartServer() {
 	r := chi.NewRouter()
+	// A good base middleware stack
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
 	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	// Set a timeout value on the request context (ctx), that will signal
+	// through ctx.Done() that the request has timed out and further
+	// processing should be stopped.
+	r.Use(middleware.Timeout(60 * time.Second))
+
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("welcome"))
 	})
