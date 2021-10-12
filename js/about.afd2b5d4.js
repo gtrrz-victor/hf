e546cdc3843a25d8d4a2e9ce7d0a71643b947994
gtrrz-victor <gtrrz.victor@gmail.com>
2021-10-12 03:52:10 +0000

    Deploying to gh-pages from @ gtrrz-victor/hf@08c2acda23b1167d5196dbdd97169bc8ff5e7289 🚀

--- js/about.afd2b5d4.js
+++ js/about.afd2b5d4.js
@@ -1,2 +1,2 @@
 (window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{9637:function(e,n,a){"use strict";a.r(n);var t=function(){var e=this,n=e.$createElement,a=e._self._c||n;return a("recipes-manager")},c=[],s=a("2b0e"),r=a("dece"),o=s["a"].extend({name:"Recipes",components:{RecipesManager:r["a"]}}),p=o,u=a("2877"),i=Object(u["a"])(p,t,c,!1,null,null,null);n["default"]=i.exports}}]);
-//# sourceMappingURL=about.5063b440.js.map
+//# sourceMappingURL=about.afd2b5d4.js.map
